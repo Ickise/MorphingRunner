4c3ae77c39eca992d6ad66f7387bc3648c5ef3fd
Ickise <wolfroox.gamer@outlook.fr>
2023-01-24 19:01:58 +0100

    Collecte d'ADN spécial et compteur de celle-ci, faite

--- Assets/Enzo/Scripts/Object To Collect/DNAToCollect.cs
+++ Assets/Enzo/Scripts/Object To Collect/DNAToCollect.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DNAToCollect : MonoBehaviour
 {
     Scoring scoring;
+
+    [SerializeField] DNA[] listDNA = new DNA[3];
+    [SerializeField] int[] listSpecialDNA = new int[3];
 
     void Start()
     {
@@ -11,10 +15,17 @@ public class DNAToCollect : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("DNA"))
         {
-            scoring.score++;
-            Destroy(gameObject);
+            for (int i = 0; i < listDNA.Length; i++)
+            {
+                if (other.name == listDNA[i].dnaName + "(Clone)")
+                {
+                    listSpecialDNA[i]++;
+                    scoring.score++;
+                    Destroy(other.gameObject);
+                }
+            }
         }
     }
 }
