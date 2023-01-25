4e99118248bbee7f1900d21ec87aea79ccec4665
Ickise <wolfroox.gamer@outlook.fr>
2023-01-25 09:30:04 +0100

    Les choix de transformations sont fait, je continue de faire l'UI pour qu'on puisse appuyer ou non quand on a plus ou moins de l'ADN

--- Assets/Enzo/Scripts/Player/DNAToCollect.cs
+++ Assets/Enzo/Scripts/Player/DNAToCollect.cs
@@ -1,16 +1,35 @@
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DNAToCollect : MonoBehaviour
 {
     Scoring scoring;
 
+
     [SerializeField] DNA[] listDNA = new DNA[3];
+
     [SerializeField] int[] listSpecialDNA = new int[3];
+
+    [SerializeField] Button[] buttonList = new Button[3];
 
     void Start()
     {
         scoring = GameObject.FindWithTag("Scoring").GetComponent<Scoring>();
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < listDNA.Length; i++)
+        {
+            if (listSpecialDNA[i] >= 10)
+            {
+                buttonList[i].interactable = true;
+            }
+            else
+            {
+                buttonList[i].interactable = false;
+            }
+        }
     }
 
     void OnTriggerEnter(Collider other)
---
+++ Assets/Enzo/Scripts/Transformation/TransformationsChoices.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using UnityEngine;
+
+public class TransformationsChoices : MonoBehaviour
+{
+    [SerializeField] GameObject transformationChoices;
+    [SerializeField] GameObject playerMesh;
+
+    [SerializeField] Mesh[] meshList = new Mesh[3];
+
+    bool slowMotionActive = false;
+
+    void Start()
+    {
+        transformationChoices.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (!slowMotionActive)
+            {
+                StartCoroutine(SlowMotion());
+            }
+            else
+            {
+                StopCoroutine(SlowMotion());
+                ReturnInRealTime();
+            }
+        }
+    }
+
+    IEnumerator SlowMotion()
+    {
+        slowMotionActive = true;
+        Time.timeScale = 0.5f;
+        transformationChoices.SetActive(true);
+
+        while (slowMotionActive)
+        {
+            yield return null;
+        }
+    }
+
+    public void ChangeMesh(int meshIndex)
+    {
+        playerMesh.GetComponent<MeshFilter>().mesh = meshList[meshIndex];
+        ReturnInRealTime();
+    }
+
+    void ReturnInRealTime()
+    {
+        Time.timeScale = 1;
+        transformationChoices.SetActive(false);
+        slowMotionActive = false;
+    }
+}
