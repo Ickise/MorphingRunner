385fe9c575cf512bfb60ab49e3ab017c259062c3
Ickise <wolfroox.gamer@outlook.fr>
2023-01-25 20:32:36 +0100

    Tag

---
+++ Assets/Enzo/Scripts/Transformation/TRexTransformation.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TRexTransformation : MonoBehaviour
+{
+    bool isTransform;
+
+    public void TRexTransfo()
+    {
+        if(isTransform)
+        {
+            
+        }
+    }
+}
