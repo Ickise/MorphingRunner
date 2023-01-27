2442650eae5eefd342ba4a461c2ce246c58155e7
Ickise <wolfroox.gamer@outlook.fr>
2023-01-27 13:37:24 +0100

    Transformation début

--- Assets/Enzo/Scripts/Transformation/TRexTransformation.cs
+++ Assets/Enzo/Scripts/Transformation/TRexTransformation.cs
@@ -8,9 +8,13 @@ public class TRexTransformation : MonoBehaviour
 
     public void TRexTransfo()
     {
+        isTransform = true;
+
         if(isTransform)
         {
-            
+            // voir dans le script de cyril pour désactiver son life et le réactiver, voir pour que je compare tag avec les 
+            // obstacles et leur mettre le tag Collider pour les casser et les détruire. Mettre le bool en true puis en false
+            // une fois qu"on change de pouvoir pour éviter d'avoir tout et jongler avec les bool. 
         }
     }
 }
