f75a9492d3da642e06c6264e316e0109b7ba6a49
cyrilkordupel <kordupelcyril@gmail.com>
2023-01-31 21:46:43 +0100

    Procédural Animation

--- Assets/Cyril/Scripts/PointMove.cs
+++ Assets/Cyril/Scripts/PointMove.cs
@@ -1,93 +1,94 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 public class PointMove : MonoBehaviour
 {
     [Header("Important")]
     [SerializeField] private Transform _refLeg;
     [SerializeField] private Transform _targetLeg;
-    [SerializeField] private Rigidbody _rigidbodyPlayer;
     [SerializeField] private LayerMask _layerMask;
     [SerializeField] private float _sphereRadius;
     [SerializeField] private float _maxDistanceShphereCast;
     [SerializeField] private float _flaotAnticipation;
 
     [Header("Donnée")]
-    [SerializeField] private bool _Contact = false;
+    private bool _Contact = false;
     [SerializeField] private bool _Once = false;
-    [SerializeField] private bool _Back = false;
-    [SerializeField] private float _time;
-    [SerializeField] private Vector3 _PointOfHit = Vector3.zero;
-    [SerializeField] private Vector3 _Déplacement;
-    [SerializeField] private float _timeCouroutine;
-    // Start is called before the first frame update
-    private void Update() 
+    public bool _Jambe = false;
+    private float _time;
+    private Vector3 _PointOfHit = Vector3.zero;
+    private Vector3 Déplacement = Vector3.zero;
+    private Vector3 _basePosition = Vector3.zero;
+    private Vector3 _newPosition = Vector3.zero;
+    [SerializeField] private Vector3 _VectorCorection = Vector3.zero;
+    [SerializeField] private PointMove _scriptPointMove;
+    [SerializeField] private float lerpSpeed;
+    private void Update()
     {
-        Déplacement();
+        if (_Once == false)
+        {
+            StartCoroutine(LerpLoopCoroutine());
+        }
         Debug.Log("Fixed");
         RaycastHit _hit;
-        Vector3 RefPos = _refLeg.position + _rigidbodyPlayer.velocity * _flaotAnticipation + _Déplacement;
-        if(Physics.SphereCast(RefPos,_sphereRadius,-transform.up,out _hit,_maxDistanceShphereCast,_layerMask))
+        Vector3 RefPos = (_refLeg.position + _VectorCorection + Déplacement) * _flaotAnticipation;
+        if (Physics.SphereCast(RefPos, _sphereRadius, -transform.up, out _hit, _maxDistanceShphereCast, _layerMask))
         {
             Debug.Log(_hit.point);
             _Contact = true;
             _PointOfHit = _hit.point;
-            
+
         }
-        if(_Contact && Vector3.Distance(_targetLeg.position,_PointOfHit) > 0.15f)
+        if (_Contact && Vector3.Distance(_targetLeg.position, _PointOfHit) > 0.15f)
         {
             _targetLeg.position = _PointOfHit;
-            // _time += Time.deltaTime;
-            // Vector3.Lerp(_targetLeg.position,_PointOfHit,_time);
         }
-        if(_time > 1.5f)
+        if (_time > 1.5f)
         {
             _time = 0;
         }
     }
-    private IEnumerator WaitFor()
+    private IEnumerator LerpLoopCoroutine()
     {
-        yield return new WaitForSeconds(_timeCouroutine);
-        Debug.Log("Active");
-        if(_Déplacement.z < 1 && _Once == false)
+        if (_Once == false && _Jambe)
         {
-            _Déplacement = _Déplacement + new Vector3(0,0,1);
+            _basePosition = transform.position + new Vector3(0, 0, -40);
+            _newPosition = new Vector3(transform.position.x, transform.position.y, -8);
             _Once = true;
-            _Back = true;
         }
-    }
-    private void Déplacement()
-    {
-        if(_Déplacement.z > 0 && _Back == false)
+        while (_Jambe)
         {
-            _Déplacement.z = _Déplacement.z - Time.deltaTime/2;
-            StartCoroutine(WaitFor());  
-        }      
-        if(_Déplacement.z < 0)
-        {
+            float elapsedTime = 0f;
+            while (elapsedTime < lerpSpeed)
+            {
+                Déplacement = Vector3.Lerp(_basePosition, _newPosition, elapsedTime / lerpSpeed);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+            yield return new WaitForSeconds(1);
+            _scriptPointMove._Jambe = true;
+            elapsedTime = 0f;
+            while (elapsedTime < lerpSpeed)
+            {
+                Déplacement = Vector3.Lerp(_newPosition, _basePosition, elapsedTime / lerpSpeed);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+            yield return new WaitForSeconds(1);
+            _Jambe = false;
             _Once = false;
         }
-        if(_Back == true)
-        {
-            _Déplacement.z = _Déplacement.z + Time.deltaTime/2;        
-        }
-        if(_Back == true && _Déplacement.z > 1)
-        {
-            _Back = false;
-        }
     }
-    private void OnDrawGizmos() 
+    private void OnDrawGizmos()
     {
-        if(_Contact == true)
+        if (_Contact == true)
         {
             Gizmos.color = Color.green;
-            Gizmos.DrawSphere(_PointOfHit,0.05f);
+            Gizmos.DrawSphere(_PointOfHit, 0.05f);
         }
         else
         {
             return;
-        }        
+        }
     }
 }
 
