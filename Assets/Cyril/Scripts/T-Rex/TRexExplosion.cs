f942d596127b475ad18cf5506b428010676440cc
cyrilkordupel <kordupelcyril@gmail.com>
2023-02-13 22:37:41 +0100

    T-Rex explosive Force

--- Assets/Cyril/Scripts/T-Rex/TRexExplosion.cs
+++ Assets/Cyril/Scripts/T-Rex/TRexExplosion.cs
@@ -17,12 +17,14 @@ public class TRexExplosion : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other) 
     {
+        Debug.Log("Tigger");
         if(!other.CompareTag("Collider"))
         {
             return;
         }
         GameObject Obstacle = other.gameObject;
         Rigidbody ObstacleRigidbody = Obstacle.GetComponent<Rigidbody>();
-       // ObstacleRigidbody.AddForce(vec)
+        ObstacleRigidbody.AddExplosionForce(20000,transform.position,5,30);
+        Debug.Log(ObstacleRigidbody.name);
     }
 }
