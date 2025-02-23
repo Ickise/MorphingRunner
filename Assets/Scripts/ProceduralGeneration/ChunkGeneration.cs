a2cf57ab374f28ed82bea30dd23ffa2a1972e5c9
Ickise <enzo.dufour05@gmail.com>
2025-02-24 00:24:03 +0100

    Commentaires de chunkgeneration et chunkdata

--- Assets/Scripts/ProceduralGeneration/ChunkGeneration.cs
+++ Assets/Scripts/ProceduralGeneration/ChunkGeneration.cs
@@ -1,11 +1,15 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ChunkGeneration : MonoBehaviour
 {
-    [SerializeField] private List<ChunkData> _listChunk = new List<ChunkData>();
-    [SerializeField] private List<ChunkData> _listChunkPause;
+    // Ce script permet de générer des chunks de manière procédurale. Il est possible de définir des chunks de pause et des chunks d'obstacles.
+    // Nous avions séparé les datas et la gestion pour plus de clarté.
+    
+    // La nomenclature n'est pas correcte, les variables commencent par _ pour indiquer qu'elles sont privées, mais cela n'est pas nécessaire.
+    // De plus, les noms de variables ne sont pas explicites.
+    [SerializeField] private List<ChunkData> _listChunk = new List<ChunkData>(); // Nous ne vérifions jamais si ces listes sont vides, cela pourrait poser problème.
+    [SerializeField] private List<ChunkData> _listChunkPause; // De même pour cette liste.
     [SerializeField] private GameObject _pointStart;
     [SerializeField] private Vector3 _spacing;
     [SerializeField] private float _spacingPlus;
@@ -31,6 +35,9 @@ public class ChunkGeneration : MonoBehaviour
     }
     private void Update()
     {
+        // Cette méthode est peu claire puisque si la position en Z est inférieur à 150, on génère un nouveau chunk sans prendre en compte la taille du chunk.
+        // Si les chunks sont de taille variable, cela peut poser un problème.
+        // De plus, l'utilisation répétée de transform.position est coûteuse en performances.
         if (_pointStart.transform.position.z < 150)
         {
             Generate();
@@ -43,6 +50,9 @@ public class ChunkGeneration : MonoBehaviour
         else return _chunkData._visual.transform.rotation;
 
     }
+    
+    // Cette méthode fait quasiment la même chose que GenerateChunkPause, il serait plus judicieux de les fusionner.
+    // Nous aurions pu créer une méthode GenerateChunk qui prend en paramètre une liste de chunks et un booléen pour savoir si nous devions faire une pause ou non.
     private void GenerateChunkObstacle()
     {
         int RandomN = Random.Range(0, _listChunk.Count);
@@ -79,14 +89,19 @@ public class ChunkGeneration : MonoBehaviour
         MovePoint();
         _movePoint = false;
     }
+    
+    // Encore une fois l'utilisation répétée de transfom.position est coûteuse en performances.
+    // Nous aurions pu simplement la stocker dans une variable Vector3. 
     private void MovePoint()
     {
         _pointStart.transform.position = _chunkFinish.transform.position + _spacing;
         MoveDecor moveDecor = _pointStart.GetComponent<MoveDecor>();
         moveDecor.SetSpeed(_speed);
     }
+    
     private void GenerationSeed()
     {
+        // Nous aurions pu utiliser un ternaire pour simplifier le code : _seed = _newSeed ? Random.Range(int.MinValue, int.MaxValue) : _mySeed;
         if (_newSeed == true)
         {
             _seed = Random.Range(int.MinValue, int.MaxValue);
@@ -98,6 +113,8 @@ public class ChunkGeneration : MonoBehaviour
 
         Random.InitState(_seed);
     }
+    
+    // Cette méthode est peu claire, mais elle permet en réalité de set up la vitesse du jeu en fonction du temps.
     private void Speed()
     {
         _time = _time + Time.deltaTime;
--- Assets/Scripts/Scriptable Object/ChunkData.cs
+++ Assets/Scripts/Scriptable Object/ChunkData.cs
@@ -1,10 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = ("ChunkData"))]
 public class ChunkData : ScriptableObject
 {
+   // Tout simplement les données de nos chunk, peut être l'une des seules choses "optimisée" dans notre projet.
+   // Encore une fois, la nomenclature n'est pas bonne avec _ devant les variables.
    public GameObject _visual;
    public GameObject _pointEntry;
    public GameObject _pointExit;
