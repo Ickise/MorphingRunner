6099d327cfbd069ae7ca0caf9f15892003fda0d9
Enzo Dufour <enzo.dufour05@gmail.com>
2025-02-26 12:08:47 +0100

    Update Scoring.cs

--- Assets/Scripts/Scoring/Scoring.cs
+++ Assets/Scripts/Scoring/Scoring.cs
@@ -3,6 +3,7 @@ using UnityEngine.UI;
 
 public class Scoring : MonoBehaviour
 {
+    // Ce script permettait d'augmenter le score du joueur mais également de mettre à jour l'UI. 
     public static Scoring scoring;
 
     public int score;
@@ -18,6 +19,7 @@ public class Scoring : MonoBehaviour
         scoring = this;
     }
 
+    // Encore une fois un manque d'optimisation puisque nous utilisions une Update pour augmenter le score et mettre à jour l'UI en permanence. 
     void Update()
     {
         timer += Time.deltaTime;
