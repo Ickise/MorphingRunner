581bb9c0531fddc20838eb4544345fc709bf7dea
Ickise <enzo.dufour05@gmail.com>
2025-03-03 20:00:16 +0100

    Update PlayerMovement.cs

--- Assets/Scripts/New/Player/PlayerMovement.cs
+++ Assets/Scripts/New/Player/PlayerMovement.cs
@@ -10,14 +10,15 @@ public class PlayerMovement : MonoBehaviour, IUpdate
     [SerializeField] private float speedIncreaseAmount = 1f;
 
     [SerializeField, Header("References")] private Transform playerTransform;
-    
+
     [SerializeField] private SideManager sideManager;
 
     private bool isMoving;
-    
+
     private Vector3 moveDirection = Vector3.forward;
 
     private float time;
+    private float targetXPosition;
 
     private GameManager gameManager;
 
@@ -26,24 +27,22 @@ public class PlayerMovement : MonoBehaviour, IUpdate
         // Abonnement à l'événement de déplacement du SideManager.
         UpdateManager.RegisterUpdate(this);
         gameManager = GameManager.instance;
-        sideManager.OnPositionChanged += MoveToPosition;
+        sideManager.OnPositionChanged += DodgeToPosition;
     }
 
     private void OnDisable()
     {
         // Désabonnement à l'événement de déplacement du SideManager. Cela permet d'éviter les références nulles lors de la destruction de l'objet.
         UpdateManager.UnregisterUpdate(this);
-        sideManager.OnPositionChanged -= MoveToPosition;
-        StopAllCoroutines();
+        sideManager.OnPositionChanged -= DodgeToPosition;
     }
 
-    private void MoveToPosition(float newPosition)
+    private void DodgeToPosition(float newPosition)
     {
         // Ici, je déclenche le déplacement vers une nouvelle position si le joueur n'est pas déjà en mouvement.
-        if (!isMoving)
-        {
-            StartCoroutine(OnDodge(newPosition));
-        }
+        targetXPosition = newPosition;
+
+        isMoving = true;
     }
 
     // Je récupère l'état de déplacement du joueur pour l'utiliser dans d'autre script
@@ -52,45 +51,42 @@ public class PlayerMovement : MonoBehaviour, IUpdate
         return isMoving;
     }
 
-    // La coroutine gère le déplacement fluide du joueur.
-    private IEnumerator OnDodge(float xPosition)
-    {
-        isMoving = true;
-
-        // Je peux améliorer cette partie en enlevant le while.
-        while (Mathf.Abs(playerTransform.position.x - xPosition) > 0.01f)
-        {
-            // Je stocke la position du joueur pour éviter les transform.position inutiles.
-            var position = playerTransform.position;
-            // Ensuite, j'introduis nous nouvelle variable qui va conserver la position du joueur.
-            var playerPosition = position;
-
-            // Le fait de mettre un MoveTowards permet de déplacer le joueur de manière fluide et constante peu importe les FPS. 
-            position = Vector3.MoveTowards(position,
-                new Vector3(xPosition, playerPosition.y, playerPosition.z), dodgeSpeed * Time.deltaTime);
-            playerTransform.position = position;
-            yield return null;
-        }
-
-        isMoving = false;
-    }
-
     // Cela permet de modifier la vitesse de dodge selon la transformation.
     public void SetDodgeSpeed(float speed)
     {
         dodgeSpeed = speed;
     }
 
+    private void Move()
+    {
+        // Je stocke la position du joueur pour éviter les transform.position inutiles.
+
+        var position = playerTransform.position;
+        position = Vector3.MoveTowards(position,
+            new Vector3(targetXPosition, position.y, position.z), dodgeSpeed * Time.deltaTime);
+        playerTransform.position = position;
+
+        if (Mathf.Abs(playerTransform.position.x - targetXPosition) <= 0.01f)
+        {
+            isMoving = false;
+        }
+    }
+
     public void UpdateTick()
     {
         if (gameManager.GameIsOver()) return;
-        
+
         time += Time.deltaTime;
-        
+
         playerTransform.position += moveDirection * (movementSpeed * Time.deltaTime);
 
-        if (time <= speedIncreaseInterval) return;
-        movementSpeed += speedIncreaseAmount;
-        time = 0f;
+        if (time >= speedIncreaseInterval)
+        {
+            movementSpeed += speedIncreaseAmount;
+            time = 0f;
+        }
+
+        if (!isMoving) return;
+        Move();
     }
 }
