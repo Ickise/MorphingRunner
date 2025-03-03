0aaf16868577068bb81e7c242751bf39adef8cf7
Ickise <enzo.dufour05@gmail.com>
2025-03-03 20:04:41 +0100

    Update UpdateManager.cs

--- Assets/Scripts/New/Manager/UpdateManager.cs
+++ Assets/Scripts/New/Manager/UpdateManager.cs
@@ -3,10 +3,16 @@ using UnityEngine;
 
 public class UpdateManager : MonoBehaviour
 {
+    // Ce script est très utile pour de l'optimisation. C'est un UpdateManager et permet de n'avoir qu'une seule Update, FixedUpdate et LateUpdate dans le jeu.
+    // Si un script hérite de l'une des interfaces IUpdate, ILateUpdate ou IFixedUpdate, alors il peut s'abonner à l'UpdateManager et bénéficier des 
+    // différentes fonctions du script UpdateManager.
+
     static HashSet<IUpdate> IUpdates = new HashSet<IUpdate>();
     static HashSet<ILateUpdate> ILateUpdates = new HashSet<ILateUpdate>();
     static HashSet<IFixedUpdate> IFixedUpdates = new HashSet<IFixedUpdate>();
 
+    
+    // Ici nous retrouvons les fonctions pour s'abonner ou se désabonner de l'Update, FixedUpdate ou LateUpdate.
     public static void RegisterUpdate(IUpdate obj)
     {
         IUpdates.Add(obj);
@@ -37,6 +43,7 @@ public class UpdateManager : MonoBehaviour
         IFixedUpdates.Remove(obj);
     }
 
+    // Ici nous retrouvons les fonctions pour lancer l'Update, la FixedUpdate ou la LateUpdate.
     private void Update()
     {
         HashSet<IUpdate>.Enumerator e = IUpdates.GetEnumerator();
